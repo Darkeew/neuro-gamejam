36cc91e982c4443a6865f87c85eefbdd33f92846
Adesi <Kopietz123@gmail.com>
2023-12-30 06:34:59 +0100

    Load All files in Dialog Directory

--- scripts/eventsystem/dialog_bus.gd
+++ scripts/eventsystem/dialog_bus.gd
@@ -12,11 +12,20 @@ extends Node
 # }
 
 func _ready():
-	#load the json file
-	var json = load("res://assets/dialog/dialog.json")
-	var dialog = json.data
-	for i in dialog:
-		#register the event
-		EventBus.register_event(i.trigger, self, "show_dialog", {"dialog":i.dialog,"next": i.next,"condition": i.condition})
+	#load all dialog files in assets/dialog
+	var json_files = []
+	var dir : PackedStringArray = DirAccess.get_files_at("res://assets/dialog")
+	for i in dir:
+		if i.ends_with(".json"):
+			json_files.append(i)
+
+	#load all json files
+	for json_file in json_files:
+		print("loading dialog file: "+json_file)
+		var json = load("res://assets/dialog/"+json_file)
+		var dialog = json.data
+		for i in dialog:
+			#register the event
+			EventBus.register_event(i.trigger, self, "show_dialog", {"dialog":i.dialog,"next": i.next,"condition": i.condition})
 	
 	EventBus.print_all()
