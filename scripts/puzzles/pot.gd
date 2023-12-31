866983047e49aaf5215d1bb64ae842b4168f8ec3
KotgeDev <68812485+KotgeDev@users.noreply.github.com>
2024-01-01 03:05:52 +0900

    fix

--- scripts/puzzles/pot.gd
+++ scripts/puzzles/pot.gd
@@ -1,10 +1,13 @@
 extends Node2D
 
+var ignore := false 
+
 func _on_area_2d_body_entered(body):
 	if not body is Player:
 		return 
 	
-	if Global.current_iteration == 4: 
+	if Global.current_iteration == 4 and not ignore:
+		ignore = true  
 		$Broken.visible = true 
 		$Sprite2D.visible = false 
 		
