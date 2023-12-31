9d47057d1504a64a8edfc9507e529f8f76e53657
Adesi <Kopietz123@gmail.com>
2023-12-31 08:06:01 +0100

    Add flag system to dialog json

--- scripts/eventsystem/test.gd
+++ scripts/eventsystem/test.gd
@@ -10,15 +10,41 @@ func _ready():
 var last_dialog = ""
 
 func show_dialog(event):
-	var d = event.dialog.pick_random()
+	var d = event.dialog[0]
+	var flags = event.flags if event.has("flags") else []
+	if !flags.has("unpaused"):
+		Global.game_paused = true
+
+	if flags.has("sequence"):
+		sequence_play(event)
+		return
+		
+	d = event.dialog.pick_random()
 	#print(event)
 	while d.text == last_dialog and event.dialog.size() > 1:
 		d = event.dialog.pick_random()
+		
 
 	text = d.text
-	Global.game_paused = true
+	
 	await get_tree().create_timer(d.time).timeout
 	text = ""
 	if event.has("time"):
 		await get_tree().create_timer(event.time).timeout
-	Global.game_paused = false
+	if flags.has("unpaused"):
+		Global.game_paused = false
+
+func sequence_play(event):
+	var sequence = event.dialog
+	var flags = event.flags if event.has("flags") else []
+	var i = 0
+	while i < sequence.size():
+		var d = sequence[i]
+		text = d.text
+		await get_tree().create_timer(d.time).timeout
+		text = ""
+		i += 1
+	if event.has("time"):
+		await get_tree().create_timer(event.time).timeout
+	if flags.has("unpaused"):
+		Global.game_paused = false
