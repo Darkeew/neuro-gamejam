2748c6aa33d80c5d9b39a1c7f8a1a3dff7061920
Adesi <Kopietz123@gmail.com>
2024-01-06 14:18:05 +0100

    Update AccurateShadow.gd

--- scripts/character/AccurateShadow.gd
+++ scripts/character/AccurateShadow.gd
@@ -1,4 +1,5 @@
-extends Node
+@tool
+extends Node2D
 
 @onready var sprite: Node2D = get_parent()
 
@@ -7,12 +8,14 @@ var copy_transform: RemoteTransform2D
 
 @export var shadow_offset: Vector2 = Vector2(0, 0)
 @export var shadow_scale_offset: Vector2 = Vector2(1, 1)
-@export var static_shadow: bool = false
 
 func _ready():
+	if Engine.is_editor_hint():
+		return
 	dummysprite2d = Sprite2D.new()
 	dummysprite2d.scale = sprite.scale
 	dummysprite2d.flip_v = true
+
 	if sprite is Sprite2D:
 		dummysprite2d.texture = sprite.texture
 		dummysprite2d.hframes = sprite.hframes
@@ -25,12 +28,20 @@ func _ready():
 	
 	Global.shadow_canvas_group.add_child.call_deferred(dummysprite2d)
 	
+func _process(delta):
+	if !Engine.is_editor_hint():
+		return
+	queue_redraw()
+
+func _exit_tree():
+	if Engine.is_editor_hint():
+		return
+	dummysprite2d.queue_free()
 
 func _physics_process(_delta):		
-	dummysprite2d.visible = sprite.visible
-
-	if static_shadow:
+	if Engine.is_editor_hint():
 		return
+	dummysprite2d.visible = sprite.visible
 
 	if dummysprite2d.is_inside_tree() and copy_transform == null:
 		copy_transform = RemoteTransform2D.new()
@@ -49,3 +60,28 @@ func _physics_process(_delta):
 		dummysprite2d.texture = sprite.sprite_frames.get_frame_texture(sprite.animation,sprite.frame)
 	else:
 		dummysprite2d.texture = null
+
+
+var editor_texture = null
+var _sprite = null
+func _draw():
+	if !Engine.is_editor_hint():
+		return
+
+	#print("Drawing shadow")
+	if _sprite == null:
+		_sprite = get_parent()
+		if _sprite is Sprite2D:
+			editor_texture = _sprite.texture
+		elif _sprite is AnimatedSprite2D:
+			editor_texture = _sprite.sprite_frames.get_frame_texture(_sprite.animation,_sprite.frame)
+		else:
+			editor_texture = null
+	
+	var r = Rect2(0,0,editor_texture.get_width(),editor_texture.get_height())
+	#center the position
+	r.position = _sprite.position + shadow_offset + _sprite.offset
+	r.position -= Vector2(0, _sprite.offset.y)*2
+	r.position -= r.size/2
+	r.size = Vector2(r.size.x, -r.size.y)
+	draw_texture_rect(editor_texture,r,false,Color(0,0,0,0.5),false)
